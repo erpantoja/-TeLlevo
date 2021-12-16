c04835c9b579e62f87148ff26c16d0cc5ecb8309
erpantoja <er.pantoja@duocuc.cl>
2021-12-16 16:24:17 -0300

    Eliminacion carpetas extra

--- src/app/services/viajes.service.ts
+++ src/app/services/viajes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { retry } from 'rxjs/operators';
 import { Viajes } from './../interfaces/viajes';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,9 @@ export class ViajesService {
 
   constructor( private http: HttpClient,) {
    }
-   getAllViajes() {
+   getAllViajes():Observable<any> {
     const path = `${this.api}`;
-    return this.http.get<Viajes[]>(path).pipe(
+    return this.http.get<any[]>(path).pipe(
       retry(3)
     );
   }
@@ -33,5 +34,12 @@ export class ViajesService {
     const path = `${this.api}${id}`;
     return this.http.delete(path)
   }
+
+  getAllViajes2():Observable<Viajes[]> {
+    const path = `${this.api}`;
+    return this.http.get<Viajes[]>(path) 
+    
+  }
+  data= Object.values(this.getAllViajes2)
 }
 
